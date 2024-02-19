977e638fd6b98ce92f89bcbb26fda8ea823c596e
candyycode <perryc@15HPEnvyx360Lap>
2024-02-18 23:00:21 -0600

    initial

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import {puppyList} from './data.js'
+import './App.css'
+
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup);
+
+  function handleClick(puppy)  {
+    console.log("puppy id: ", puppy.id);
+    setFeatPupId(puppy.id);
+  }
+
+  return (
+    <div className='App'>
+      { 
+        puppies.map((puppy) => {
+          return <p onClick={() => handleClick(puppy)} key={puppy.id}>{puppy.name}</p>
+        })
+      }
+      {featPupId && <div>
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+      </div>}
+    </div>
+  )
+}
+
+export default App
